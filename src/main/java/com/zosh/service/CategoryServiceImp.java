edd5d932a664245f17094800c8808e86fecf95e2
Amit Yadav <amit931552@gmail.com>
2025-04-12 23:28:13 +0530

    hkllkfsdj;lfd

--- src/main/java/com/zosh/service/CategoryServiceImp.java
+++ src/main/java/com/zosh/service/CategoryServiceImp.java
@@ -1,6 +1,9 @@
 package com.zosh.service;
 
 import com.zosh.model.Category;
+import com.zosh.model.Restaurant;
+import com.zosh.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,16 +11,26 @@ import java.util.List;
 @Service
 public class CategoryServiceImp implements CategoryService {
 
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
 
     @Override
-    public Category createCategory(String name, Long userId) {
-        return null;
+    public Category createCategory(String name, Long userId) throws Exception {
+        Restaurant restaurant=restaurantService.findRestaurantById(userId);
+        Category category=new Category();
+        category.setName(name);
+        category.setRestaurant(restaurant);
+        return categoryRepository.save(category);
     }
 
     @Override
     public List<Category> findCategoryByRestaurantId(Long id) throws Exception {
-        return List.of();
+
+        return categoryRepository.findByRestaurantId(id);
     }
 
     @Override
