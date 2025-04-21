8acfb42ee529269988453deb3ef1bc32bfe3b073
Amit Yadav <amit931552@gmail.com>
2025-04-21 11:31:37 +0530

    ghksnld

--- src/main/java/com/zosh/service/OrderServiceImpl.java
+++ src/main/java/com/zosh/service/OrderServiceImpl.java
@@ -1,14 +1,36 @@
 package com.zosh.service;
 
+import com.zosh.model.Address;
 import com.zosh.model.Order;
 import com.zosh.model.User;
+import com.zosh.repository.AddressRepository;
+import com.zosh.repository.OrderRepository;
+import com.zosh.repository.OrderitemRepository;
 import com.zosh.request.OrderRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class OrderServiceImpl implements  OrderService{
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderitemRepository orderitemRepository;
+
+    @Autowired
+    private AddressRepository addressRepository;
+
     @Override
     public Order createOrder(OrderRequest order, User user) {
+        Address shippAddress=order.getDeliveryAddress();
+
+        Address saveAddress=addressRepository.save(shippAddress);
+
+        if(!user.getAddresses().contains(saveAddress)){
+            user.getAddresses().add(saveAddress);
+        }
         return null;
     }
 
